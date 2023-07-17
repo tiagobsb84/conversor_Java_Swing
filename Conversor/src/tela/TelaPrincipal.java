12c93e3c4dc9c297082ef86bcfbefca8910d8658
Tiago Silva <tiagobsb84@gmail.com>
2023-07-17 02:21:20 -0300

    Adicionando imagem de icone no menu principal

--- Conversor/src/tela/TelaPrincipal.java
+++ Conversor/src/tela/TelaPrincipal.java
@@ -1,10 +1,15 @@
-package com.tiago.conversor.tela;
+package tela;
 
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 public class TelaPrincipal {
 
 	public static void main(String[] args) {
+		
+		//Imagens para usar como icones
+		ImageIcon iconAlura = new ImageIcon("src/imagem/A_do_alura.jpg");
+		
 		
 		Integer escolha = 0;
 		String[] menuOpcoes = {"Conversor de Moedas", "Conversor de Temperatura"};
@@ -12,7 +17,7 @@ public class TelaPrincipal {
 		do {
 			//Painel do menu com a opção de conversor
 			String telaMenu = (String) JOptionPane.showInputDialog(null, "Escolha uma opção", "Menu Conversor",
-					JOptionPane.QUESTION_MESSAGE, null, menuOpcoes, menuOpcoes[0]);
+					JOptionPane.QUESTION_MESSAGE, iconAlura, menuOpcoes, menuOpcoes[0]);
 			
 			//para sair do menu se clicar no x
 			if(telaMenu == null) {
