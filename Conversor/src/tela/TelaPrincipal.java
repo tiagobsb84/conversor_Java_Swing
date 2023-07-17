f1425ba9ed503224a7e536816381d9dc798d4875
Tiago Silva <tiagobsb84@gmail.com>
2023-07-17 03:20:12 -0300

    Painel de validacao do input valor

--- Conversor/src/tela/TelaPrincipal.java
+++ Conversor/src/tela/TelaPrincipal.java
@@ -10,10 +10,12 @@ public class TelaPrincipal {
 		//Imagens para usar como icones
 		ImageIcon iconAlura = new ImageIcon("src/imagem/A_do_alura.jpg");
 		ImageIcon iconValorInput = new ImageIcon("src/imagem/input_valor.jpg");
+		ImageIcon iconValorNumerio = new ImageIcon("src/imagem/valor_numerico.png");
 		
 		String[] menuOpcoes = {"Conversor de Moedas", "Conversor de Temperatura"};
 		Integer escolha = 0;
 		String inputValor = "";
+		Double valorCotacao = 0.0;
 		
 		do {
 			//Painel do menu com a opção de conversor
@@ -27,10 +29,21 @@ public class TelaPrincipal {
 			} else if(telaMenu.equalsIgnoreCase("Conversor de Moedas")) {
 				try {
 					while(true) {
+						//Painel do valor do input
 						inputValor = (String) (JOptionPane.showInputDialog(null, "Insira um valor", "Valor da conversão",
 								JOptionPane.QUESTION_MESSAGE,iconValorInput, null, null));
 						if(inputValor == null) {
 							System.exit(0);
+						}
+						try {
+							//Convertendo para double
+							valorCotacao = Double.parseDouble(inputValor);
+							break;
+							
+						} catch(NumberFormatException e) {
+							//Painel de valor inválido 
+							JOptionPane.showMessageDialog(null, "Insira um valor numérico.", "Mensagem Valor Inválido!", 
+									JOptionPane.INFORMATION_MESSAGE, iconValorNumerio);
 						}
 					}
 					
